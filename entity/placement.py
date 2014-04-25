559fc18943df47f3961000ebee9045b92f8d409e
paulczekalla <paul.czekalla+dev@gmail.com>
2014-04-25 14:53:33 +0200

    Added properties to placement entity

--- entity/placement.py
+++ entity/placement.py
@@ -1,26 +1,65 @@
 class Placement:
     def __init__(self, id, name, code, publisher_name, site_name, default_referrer_url, custom_categories):
-        self.id = id
-        self.name = name
-        self.code = code
-        self.publisher_name = publisher_name
-        self.site_name = site_name
-        self.default_referrer_url = default_referrer_url
-        self.custom_categories = custom_categories
-
-    def getAllEntitiesByType(self, type):
-        firstReturn = self.http.getRequestPage(0, type).json()['response']
-        count = firstReturn['count']
-
-        allEntities = list()
-        # very ugly hack with the add of the letter s
-        # for later versions maybe a dict with every plural version
-        allEntities.extend(firstReturn[type+'s'])
+        pass
         
-        print('Getting all ' + str(count) + ' items\n')
-        if count > 100:
-            for start_element in range(100, count, 100):
-                # again this plural s implementation 
-                allEntities.extend(self.http.getRequestPage(start_element, type).json()['response'][type+'s'])
-        
-        return allEntities
+    def setId(self, id):
+        self._id = id
+    
+    def getId(self):
+        return self._id
+
+    _id = property(getId, setId)
+    
+    
+    def setName(self, name):
+        self._name = name
+    
+    def getName(self):
+        return self._name
+
+    _name = property(getName, setName)
+    
+    
+    def setCode(self, code):
+        self._code = code
+    
+    def getCode(self):
+        return self._code
+
+    _code = property(getCode, setCode)
+    
+    
+    def setPublisherName(self, _publisher_name):
+        self._publisher_name = publisher_name
+    
+    def getPublisherName(self):
+        return self._publisher_name
+
+    _publisher_name = property(getPublisherName, setPublisherName)
+    
+    
+    def setSite_name(self, site_name):
+        self._site_name = site_name
+    
+    def getSite_name(self):
+        return self._site_name
+
+    _site_name = property(getSite_name, setSite_name)
+    
+    
+    def setDefault_referrer_url(self, default_referrer_url):
+        self._default_referrer_url = default_referrer_url
+    
+    def getDefault_referrer_url(self):
+        return self._default_referrer_url
+
+    _default_referrer_url = property(getDefault_referrer_url, setDefault_referrer_url)
+    
+    
+    def setCustom_categories(self, custom_categories):
+        self._custom_categories = custom_categories
+    
+    def getCustom_categories(self):
+        return self._custom_categories
+
+    _custom_categories = property(getCustom_categories, setCustom_categories)
