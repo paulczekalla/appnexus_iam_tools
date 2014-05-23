8d3f85c0ccbad199c9f79168700c353126e7d2bb
Paul Czekalla <p.czekalla@interactivemedia.net>
2014-05-23 18:01:53 +0200

    ID Mapping Skript

---
+++ get_all_entities_names.py
@@ -0,0 +1,41 @@
+﻿import json
+import os
+from lib.auth import Auth, AuthException
+from lib.httpHandler import HttpHandler
+from worker.generic.AbstractGenericWorker import AbstractGenericWorker
+
+def main():
+
+    http = HttpHandler("http://api.appnexus.com")
+    a = Auth()
+    a.aquireAuthToken(http)
+
+    #entity_list = ('platform-member', 'advertiser', 'line-item', 'campaign', 'publisher', 'site', 'placement')
+    entity_list = ('advertiser', 'publisher')
+
+    for entity in entity_list: 
+        getEntities(http, entity)
+
+
+def getEntities(http, entity_type):
+    genericWorker = AbstractGenericWorker(http)
+    entities = genericWorker.getAllEntitiesByType(entity_type)
+    writeIntoFile(entities, entity_type)
+
+
+def writeIntoFile(entities, entity_type):
+    foldername = 'id_mapping'
+    filename = 'mapping_' + entity_type + '.csv'
+    
+    if not os.path.exists(foldername):
+        os.mkdir(foldername)
+    
+    with open(foldername + '/' + filename, 'w') as fw:    
+        fw.write('id; name\n')
+
+        for entity in entities:
+            fw.write(str(entity['id']) + '; ' + entity['name'] + '\n')
+
+
+if __name__ == "__main__":
+    main()
--- worker/generic/AbstractGenericWorker.py
+++ worker/generic/AbstractGenericWorker.py
@@ -1,74 +1,71 @@
 class AbstractGenericWorker:
 	def __init__(self, http):
-		self.http = http
+		self._http = http
+
+
+	# very ugly hack with the add of the letter s
+	# for later versions maybe a dict with every plural version	
+	def getEntityPlural(self, type):
+		return type+'s'
+
 
 	def getAllEntitiesByType(self, type):
-		firstReturn = self.http.getRequestPage(0, type).json()['response']
+		firstReturn = self._http.getRequestPage(0, type).json()['response']
 		count = firstReturn['count']
 
 		allEntities = list()
-		# very ugly hack with the add of the letter s
-		# for later versions maybe a dict with every plural version
-		allEntities.extend(firstReturn[type+'s'])
+		
+		type_plural = self.getEntityPlural(type)
+		allEntities.extend(firstReturn[type_plural])
 		
 		print('Getting all ' + str(count) + ' items\n')
 		if count > 100:
 			for start_element in range(100, count, 100):
-				# again this plural s implementation 
-				allEntities.extend(self.http.getRequestPage(start_element, type).json()['response'][type+'s'])
+				allEntities.extend(self._http.getRequestPage(start_element, type).json()['response'][type_plural])
 		
 		return allEntities
+
+
+	# filtering over parameters
+	def getAllEntitiesFiltered(self, type, params):
+		firstReturn = self._http.getRequest(type, params).json()['response']
+		count = firstReturn['count']
+		
+		allEntities = list()
+		
+		type_plural = self.getEntityPlural(type)
+		allEntities.extend(firstReturn[type_plural])
+		
+		print('Getting ' + str(count) + ' items\n')
+		if count > 100:
+			for cur_start_element in range(100, count, 100):
+				allEntities.extend(self._http.getRequestPage(cur_start_element, type, params).json()['response'][type_plural])
+		
+		return allEntities
+
 
 	def getAllEntitiesByRange(self, type, start_element, stop_element):
-		firstReturn = self.http.getRequestPage(start_element, type).json()['response']
+		firstReturn = self._http.getRequestPage(start_element, type).json()['response']
 		count = firstReturn['count']
 
 		allEntities = list()
-		# very ugly hack with the add of the letter s
-		# for later versions maybe a dict with every plural version
-		allEntities.extend(firstReturn[type+'s'])
 		
+		type_plural = self.getEntityPlural(type)
+		allEntities.extend(firstReturn[type_plural])
+		
+		print('Getting ' + str(count) + ' items\n')
 		if count > 100:
 			for cur_start_element in range(start_element+100, stop_element, 100):
-				# again this plural s implementation 
-				allEntities.extend(self.http.getRequestPage(cur_start_element, type).json()['response'][type+'s'])
+				allEntities.extend(self._http.getRequestPage(cur_start_element, type).json()['response'][type_plural])
 		
 		return allEntities
+
 
 	def getAllEntitiesBySearchTerm(self, type, searchterm):
-
 		params = {'search':searchterm}
+		self.getAllEntitiesFiltered(type, params)
 
-		firstReturn = self.http.getRequest(type, params).json()['response']
-		count = firstReturn['count']
-		
-		allEntities = list()
-		# very ugly hack with the add of the letter s
-		# for later versions maybe a dict with every plural version
-		allEntities.extend(firstReturn[type+'s'])
-		
-		if count > 100:
-			for cur_start_element in range(100, count, 100):
-				# again this plural s implementation 
-				allEntities.extend(self.http.getRequestPage(cur_start_element, type, params).json()['response'][type+'s'])
-		
-		return allEntities
 
 	def getAllEntitiesRunOnOrAfterADate(self, type, first_run_date):
-
 		params = {'min_first_run':first_run_date, 'flight_info':'true'}
-
-		firstReturn = self.http.getRequest(type, params).json()['response']
-		count = firstReturn['count']
-		
-		allEntities = list()
-		# very ugly hack with the add of the letter s
-		# for later versions maybe a dict with every plural version
-		allEntities.extend(firstReturn[type+'s'])
-		
-		if count > 100:
-			for cur_start_element in range(100, count, 100):
-				# again this plural s implementation 
-				allEntities.extend(self.http.getRequestPage(cur_start_element, type, params).json()['response'][type+'s'])
-		
-		return allEntities
+		self.getAllEntitiesFiltered(type, params)
