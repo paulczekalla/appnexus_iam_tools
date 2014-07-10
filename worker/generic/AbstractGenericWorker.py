764aaffc6060b70ac2201e16927d503798f17cbf
Paul Czekalla <p.czekalla@interactivemedia.net>
2014-07-10 09:53:32 +0200

    Added first exception in entity plural generator

--- worker/generic/AbstractGenericWorker.py
+++ worker/generic/AbstractGenericWorker.py
@@ -1,82 +1,86 @@
 class AbstractGenericWorker:
-	def __init__(self, http):
-		self._http = http
+    def __init__(self, http):
+        self._http = http
 
 
-	# very ugly hack with the add of the letter s
-	# for later versions maybe a dict with every plural version	
-	def getEntityPlural(self, type):
-		return type+'s'
+    # very ugly hack with the add of the letter s
+    # for later versions maybe a dict with every plural version 
+    def getEntityPlural(self, type):
+        print(type)
+        if type == 'category':
+            return 'categories'
+        else:
+            return type+'s'
 
 
-	def getAllEntitiesByType(self, type):
-		firstReturn = self._http.getRequestPage(0, type).json()['response']
-		count = firstReturn['count']
+    def getAllEntitiesByType(self, type):
+        firstReturn = self._http.getRequestPage(0, type).json()['response']
+        count = firstReturn['count']
 
-		allEntities = list()
-		
-		type_plural = self.getEntityPlural(type)
-		allEntities.extend(firstReturn[type_plural])
-		
-		print('Getting all ' + str(count) + ' items\n')
-		if count > 100:
-			for start_element in range(100, count, 100):
-				allEntities.extend(self._http.getRequestPage(start_element, type).json()['response'][type_plural])
-		
-		return allEntities
-
-
-	# filtering over parameters
-	def getAllEntitiesFiltered(self, type, params):
-		firstReturn = self._http.getRequest(type, params).json()['response']
-		count = firstReturn['count']
-		
-		allEntities = list()
-		
-		type_plural = self.getEntityPlural(type)
-		allEntities.extend(firstReturn[type_plural])
-		
-		print('Getting ' + str(count) + ' items\n')
-		if count > 100:
-			for cur_start_element in range(100, count, 100):
-				allEntities.extend(self._http.getRequestPage(cur_start_element, type, params).json()['response'][type_plural])
-		
-		return allEntities
+        allEntities = list()
+        
+        type_plural = self.getEntityPlural(type)
+        allEntities.extend(firstReturn[type_plural])
+        
+        print('Getting all ' + str(count) + ' items\n')
+        if count > 100:
+            for start_element in range(100, count, 100):
+                allEntities.extend(self._http.getRequestPage(start_element, type).json()['response'][type_plural])
+        
+        return allEntities
 
 
-	def getAllEntitiesByRange(self, type, start_element, stop_element):
-		firstReturn = self._http.getRequestPage(start_element, type).json()['response']
-		count = firstReturn['count']
-
-		allEntities = list()
-		
-		type_plural = self.getEntityPlural(type)
-		allEntities.extend(firstReturn[type_plural])
-		
-		print('Getting ' + str(count) + ' items\n')
-		if count > 100:
-			for cur_start_element in range(start_element+100, stop_element, 100):
-				allEntities.extend(self._http.getRequestPage(cur_start_element, type).json()['response'][type_plural])
-		
-		return allEntities
-
-
-	def getAllEntitiesBySearchTerm(self, type, searchterm):
-		params = {'search':searchterm}
-		return self.getAllEntitiesFiltered(type, params)
+    # filtering over parameters
+    def getAllEntitiesFiltered(self, type, params):
+        firstReturn = self._http.getRequest(type, params).json()['response']
+        count = firstReturn['count']
+        
+        allEntities = list()
+        
+        type_plural = self.getEntityPlural(type)
+        allEntities.extend(firstReturn[type_plural])
+        
+        print('Getting ' + str(count) + ' items\n')
+        if count > 100:
+            for cur_start_element in range(100, count, 100):
+                allEntities.extend(self._http.getRequestPage(cur_start_element, type, params).json()['response'][type_plural])
+        
+        return allEntities
 
 
-	def getAllEntitiesRunOnOrAfterADate(self, type, first_run_date):
-		params = {'min_first_run':first_run_date, 'flight_info':'true'}
-		return self.getAllEntitiesFiltered(type, params)
+    def getAllEntitiesByRange(self, type, start_element, stop_element):
+        firstReturn = self._http.getRequestPage(start_element, type).json()['response']
+        count = firstReturn['count']
+
+        allEntities = list()
+        
+        type_plural = self.getEntityPlural(type)
+        allEntities.extend(firstReturn[type_plural])
+        
+        print('Getting ' + str(count) + ' items\n')
+        if count > 100:
+            for cur_start_element in range(start_element+100, stop_element, 100):
+                allEntities.extend(self._http.getRequestPage(cur_start_element, type).json()['response'][type_plural])
+        
+        return allEntities
 
 
-	def getProfileFromEntity(self, entity):
-		if entity['profile_id'] is not None:
-			profile_id = entity['profile_id']
-			params = {'id':str(profile_id)}
-			
-			return self._http.getRequest('profile', params).json()['response']['profile']
+    def getAllEntitiesBySearchTerm(self, type, searchterm):
+        params = {'search':searchterm}
+        return self.getAllEntitiesFiltered(type, params)
 
-		else:
-			print('Invalid Entity - No profile provided')
+
+    def getAllEntitiesRunOnOrAfterADate(self, type, first_run_date):
+        params = {'min_first_run':first_run_date, 'flight_info':'true'}
+        return self.getAllEntitiesFiltered(type, params)
+
+
+    def getProfileFromEntity(self, entity):
+        if entity['profile_id'] is not None:
+            profile_id = entity['profile_id']
+            params = {'id':str(profile_id)}
+            
+            return self._http.getRequest('profile', params).json()['response']['profile']
+
+        else:
+            print('Invalid Entity - No profile provided')
